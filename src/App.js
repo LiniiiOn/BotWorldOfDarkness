db0e8c0b404bcdeade5a42d919d69945461cf8fc
LiniiiOn <hilkovich380@gmail.com>
2025-11-13 23:44:32 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect} from 'react';
 import './App.css';
 import { useTelegram } from './hook/useTelegram';
+import Header from './components/Header/header';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
