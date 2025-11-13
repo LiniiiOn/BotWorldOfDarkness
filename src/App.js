808a0d6f6be7e9d891023d6c4ed44d8da336e635
LiniiiOn <hilkovich380@gmail.com>
2025-11-13 23:36:49 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect} from 'react';
 import './App.css';
 import { useTelegram } from './hook/useTelegram';
+import Header from './components/Header/header';
 
 
 function App() {
